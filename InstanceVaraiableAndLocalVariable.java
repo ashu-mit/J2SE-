7a35c86b85c1e11e25f5e3dd77be44ba08e02010
ashuCts <aashutoshjoshi110@gmail.com>
2023-02-04 17:13:39 +0530

    Instance Varaiable And Local Variable

---
+++ InstanceVaraiableAndLocalVariable.java
@@ -0,0 +1,18 @@
+public class InstanceVaraiableAndLocalVariable {
+    String name="Aashu";  //Instance variable
+    int age=21;
+    public static void main(String[] args) {
+        String lastName="Gupta";   //Local Variable
+
+        InstanceVaraiableAndLocalVariable obj1=new InstanceVaraiableAndLocalVariable();
+        InstanceVaraiableAndLocalVariable obj2=new InstanceVaraiableAndLocalVariable();
+        obj1.name="Ankit";
+        obj1.age=23;
+
+        System.out.println(obj1.name+" "+lastName);
+        System.out.println(obj1.age );
+        System.out.println(obj2.name+" "+lastName);
+        System.out.println(obj2.age );
+    }
+}
+
