921d7a020a893bd6d4ed24ac7087c29f84c07a3e
ashuCts <aashutoshjoshi110@gmail.com>
2023-02-14 17:51:51 +0530

    Guess game project

---
+++ GameProject/GusserGame/Lauch1.java
@@ -0,0 +1,82 @@
+package GusserGame;
+
+import java.util.Scanner;
+
+public class Lauch1 {
+
+    public static void main(String[] args) {
+        umpire ump=new umpire();
+        ump.InputFromGusser();
+        ump.InputFromPlayer();
+        ump.Compare();
+    }
+}
+
+class Gusser
+{
+    int guessNum;
+
+    int GuessTheNumber()
+    {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Gusser,Guess a Nummber and provide the input");
+        guessNum=sc.nextInt();
+        return guessNum;
+    }
+}
+
+class Player
+{
+    int guessNum;
+
+    int GuessTheNumber()
+    {
+        System.out.println("Player,Guess a Number and provide the input");
+        Scanner sc=new Scanner(System.in);
+        guessNum=sc.nextInt();
+        return guessNum;
+    }
+}
+
+class umpire
+{
+    int guessNumByPlayer1;
+    int guessNumByPlayer2;
+    int guessNumByPlayer3;
+    int guessNumByGusser;
+
+    void InputFromGusser()
+    {
+        Gusser g=new Gusser();
+        guessNumByGusser=g.GuessTheNumber();
+    }
+    void InputFromPlayer()
+    {
+        Player p1=new Player();
+        Player p2=new Player();
+        Player p3=new Player();
+        guessNumByPlayer1=p1.GuessTheNumber();
+        guessNumByPlayer2=p2.GuessTheNumber();
+        guessNumByPlayer3=p3.GuessTheNumber();
+    }
+    void Compare()
+    {
+        if(guessNumByGusser==guessNumByPlayer1)
+        {
+            System.out.println("player 1 is winner");
+        }
+        else if(guessNumByGusser==guessNumByPlayer2)
+        {
+            System.out.println("player 2 is winner");
+        }
+        else if(guessNumByGusser==guessNumByPlayer3)
+        {
+            System.out.println("player 3 is winner");
+        }
+        else
+        {
+            System.out.println("No one is winner");
+        }
+    }
+}
