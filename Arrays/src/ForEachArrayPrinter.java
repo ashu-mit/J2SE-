974706fde83b4a9c93b71669e3b65cdd587a92a0
ashuCts <aashutoshjoshi110@gmail.com>
2023-02-16 15:04:50 +0530

    Array programs

---
+++ Arrays/src/ForEachArrayPrinter.java
@@ -0,0 +1,8 @@
+public class ForEachArrayPrinter {
+    public static void main(String[] args) {
+        int a[]={3,12,45,22,9};
+        for (int num:a) {
+            System.out.print(num+" ");
+        }
+    }
+}
