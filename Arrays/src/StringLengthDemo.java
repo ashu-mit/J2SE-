e1f94ab2159c0be4a97297068e60c0984300eb50
ashuCts <aashutoshjoshi110@gmail.com>
2023-02-17 15:41:13 +0530

    Length method for String

---
+++ Arrays/src/StringLengthDemo.java
@@ -0,0 +1,7 @@
+public class StringLengthDemo {
+    public static void main(String[] args) {
+        String name="Aashu";
+
+                System.out.println(name.length());  //in array lenth is property but in string it is method
+    }
+}
