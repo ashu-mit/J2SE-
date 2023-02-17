6704ee8b88c507504e9fbc1b0610d4343a4f6c10
ashuCts <aashutoshjoshi110@gmail.com>
2023-02-17 15:43:11 +0530

    Use length property in maxtix array

---
+++ Arrays/src/TwoDArrayLengthDemo.java
@@ -0,0 +1,10 @@
+public class TwoDArrayLengthDemo {
+    public static void main(String[] args) {
+        int nums[][]={
+                {3,5,1,2},          //we have 3 element in this array for nums[0](1st elemnet length is 4
+                {5,2,1,0},
+                {2,7,0,4}
+        };
+        System.out.println(nums[0].length);
+    }
+}
